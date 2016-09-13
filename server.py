4087793ef32bd2e44d7055314a3a26b9b96d6af7
sensible-heart <alexandra.makepeace@gmail.com>
2016-09-12 20:50:34 -0600

    Added handling for outputting index.html file in a blank request.

--- server.py
+++ server.py
@@ -28,11 +28,31 @@ import SocketServer
 
 
 class MyWebServer(SocketServer.BaseRequestHandler):
+    def determine_returned_page(self,request_page):
+        print("This is yo request_page foo! " + request_page)
+        if request_page == "/":
+            index_html = open('www/index.html','r')
+            page = index_html.read()
+            #print(page)
+            self.request.sendall(page)
+    
+    def parse_request(self,data):
+        divided_data = self.data.split()
+        http_method = divided_data[0]
+        http_request_page = divided_data[1]
+        http_protocol = divided_data[2]
+        requester = divided_data[4]
+        host = divided_data[6]
+        accept = divided_data[8]
+        self.determine_returned_page(http_request_page)
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+        #self.request.sendall("OK")
+        self.parse_request(self.data)
+            
+            
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
